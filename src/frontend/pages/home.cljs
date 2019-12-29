9b9a11b9c523bf3ba987c73a8d6368599a30aa85
Sergey Andronov <riddlerrr@gmail.com>
2019-12-29 20:44:23 +0300

    Fill home page a bit

--- src/frontend/pages/home.cljs
+++ src/frontend/pages/home.cljs
@@ -2,4 +2,15 @@
 
 (defn page-content []
   [:div
-   [:h2 "Welcome to my personal page"]])
+   [:h2 "Sergey Andronov, Ruby on Rails developer"]
+   [:p "I am a full stack developer with 7+ years of " 
+    [:mark "Ruby on Rails"] 
+    " experience in commercial projects."]
+   [:p "Last few years I also work with "
+    [:mark "Clojure and ClojureScript"]
+    ". BTW this site works on "
+    [:mark "Clojure + ClojureScript"] 
+    " stack and it "
+    [:a {:href "https://github.com/Riddlerrr/personal-page" :target "_blank"}
+     "open-source"]
+    "."]])
