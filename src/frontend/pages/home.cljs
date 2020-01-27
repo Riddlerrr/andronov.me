5e22bcf64096868f760edb54399bbfa49b32f256
Sergey Andronov <riddlerrr@gmail.com>
2020-01-27 17:12:39 +0300

    Fill home page content

--- src/frontend/pages/home.cljs
+++ src/frontend/pages/home.cljs
@@ -1,16 +1,32 @@
 (ns frontend.pages.home)
 
+(defn ContactsBlock []
+  [:div.contacts
+   [:h3 "My contacts:"]
+   [:a {:href "https://www.linkedin.com/in/sergey-andronov-806586b0/"}
+    "LinkedIn"]
+   ", "
+   [:a {:href "https://t.me/Riddlerrr"}
+    "Telegram"]])
+
 (defn page-content []
   [:<>
    [:h2 "Sergey Andronov, Ruby on Rails developer"]
-   [:p "I am a full stack developer with 7+ years of " 
-    [:mark "Ruby on Rails"] 
+   [:p "I am a full stack developer with 7+ years of "
+    [:mark "Ruby on Rails"]
     " experience in commercial projects."]
    [:p "Last few years I also work with "
-    [:mark "Clojure and ClojureScript"]
+    [:mark "Clojure"]
+    " and "
+    [:mark "ClojureScript"]
     ". BTW this site works on "
-    [:mark "Clojure + ClojureScript"] 
+    [:mark "Clojure + ClojureScript"]
     " stack and it "
-    [:a {:href "https://github.com/Riddlerrr/personal-page" :target "_blank"}
+    [:a {:href   "https://github.com/Riddlerrr/personal-page"
+         :target "_blank"}
      "open-source"]
-    "."]])
+    "."]
+   [:p "I am living in Russia, "
+    [:a {:href "https://en.wikipedia.org/wiki/Arzamas"} "Arzamas"]
+    " and work remotely."]
+   (ContactsBlock)])
